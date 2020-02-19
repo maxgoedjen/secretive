d0ab8b44b51194584ea7b3403ed5529bb38b141b
Max Goedjen <max.goedjen@gmail.com>
2020-02-18 19:36:41 -0800

    Setup

---
+++ SecretKit/SecretKit.h
@@ -0,0 +1,19 @@
+//
+//  SecretKit.h
+//  SecretKit
+//
+//  Created by Max Goedjen on 2/18/20.
+//  Copyright © 2020 Max Goedjen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecretKit.
+FOUNDATION_EXPORT double SecretKitVersionNumber;
+
+//! Project version string for SecretKit.
+FOUNDATION_EXPORT const unsigned char SecretKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecretKit/PublicHeader.h>
+
+
---
+++ Secretive/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Secretive
+//
+//  Created by Max Goedjen on 2/18/20.
+//  Copyright © 2020 Max Goedjen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
