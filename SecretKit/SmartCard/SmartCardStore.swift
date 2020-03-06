610bcfe02392424a489d08fab3931b9c5444e24d
Max Goedjen <max.goedjen@gmail.com>
2020-03-06 00:52:44 -0800

    Smart card working

--- SecretKit/SmartCard/SmartCardStore.swift
+++ SecretKit/SmartCard/SmartCardStore.swift
@@ -12,25 +12,102 @@ extension SmartCard {
         public let name = NSLocalizedString("Smart Card", comment: "Smart Card")
         @Published public fileprivate(set) var secrets: [Secret] = []
         fileprivate let watcher = TKTokenWatcher()
+        fileprivate var id: String?
 
         public init() {
-            watcher.setInsertionHandler { (string) in
+            id = watcher.tokenIDs.filter { !$0.contains("setoken") }.first
+            watcher.setInsertionHandler { string in
+                guard self.id == nil else { return }
                 guard !string.contains("setoken") else { return }
-                let driver = TKSmartCardTokenDriver()
-                let token = TKToken(tokenDriver: driver, instanceID: string)
-                let session = TKSmartCardTokenSession(token: token)
-                print(session)
-
+                self.id = string
+                self.secrets.removeAll()
+                self.loadSecrets()
             }
-            print(watcher.tokenIDs)
+            loadSecrets()
         }
 
-        public func sign(data: Data, with secret: SmartCard.Secret) throws -> Data {
-            fatalError()
+        // MARK: Public API
+
+        public func create(name: String) throws {
+            fatalError("Keys must be created on the smart card.")
         }
 
-        public func delete(secret: SmartCard.Secret) throws {
+        public func delete(secret: Secret) throws {
+            fatalError("Keys must be deleted on the smart card.")
+        }
+
+        public func sign(data: Data, with secret: SecretType) throws -> Data {
+            guard let id = id else { fatalError() }
+            let attributes = [
+                kSecClass: kSecClassKey,
+                kSecAttrKeyClass: kSecAttrKeyClassPrivate,
+                kSecAttrApplicationLabel: secret.id as CFData,
+                kSecAttrTokenID: id,
+                kSecReturnRef: true
+                ] as CFDictionary
+            var untyped: CFTypeRef?
+            let status = SecItemCopyMatching(attributes, &untyped)
+            if status != errSecSuccess {
+                throw KeychainError(statusCode: status)
+            }
+            guard let untypedSafe = untyped else {
+                throw KeychainError(statusCode: errSecSuccess)
+            }
+            let key = untypedSafe as! SecKey
+            var signError: SecurityError?
+            guard let signature = SecKeyCreateSignature(key, .ecdsaSignatureMessageX962SHA256, data as CFData, &signError) else {
+                throw SigningError(error: signError)
+            }
+            return signature as Data
         }
 
     }
+
+}
+
+extension SmartCard.Store {
+
+    fileprivate func loadSecrets() {
+        guard let id = id else { return }
+        let attributes = [
+            kSecClass: kSecClassKey,
+            kSecAttrTokenID: id,
+            kSecReturnRef: true,
+            kSecMatchLimit: kSecMatchLimitAll,
+            kSecReturnAttributes: true
+            ] as CFDictionary
+        var untyped: CFTypeRef?
+        let status = SecItemCopyMatching(attributes, &untyped)
+        print(status)
+        guard let typed = untyped as? [[CFString: Any]] else { return }
+        let wrapped: [SmartCard.Secret] = typed.map {
+            let name = $0[kSecAttrLabel] as? String ?? "Unnamed"
+            let id = $0[kSecAttrApplicationLabel] as! Data
+            let publicKeyRef = $0[kSecValueRef] as! SecKey
+            let publicKeySecRef = SecKeyCopyPublicKey(publicKeyRef)!
+            let publicKeyAttributes = SecKeyCopyAttributes(publicKeySecRef) as! [CFString: Any]
+            let publicKey = publicKeyAttributes[kSecValueData] as! Data
+            return SmartCard.Secret(id: id, name: name, publicKey: publicKey)
+        }
+        secrets.append(contentsOf: wrapped)
+    }
+
+}
+
+extension SmartCard {
+
+    public struct KeychainError: Error {
+        public let statusCode: OSStatus
+    }
+
+    public struct SigningError: Error {
+        public let error: SecurityError?
+    }
+
+}
+
+extension SmartCard {
+
+    public typealias SecurityError = Unmanaged<CFError>
+
 }
