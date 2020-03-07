fb752240b55bcec3f7717451fb3d89335ea30d1d
Max Goedjen <max.goedjen@gmail.com>
2020-03-07 13:43:57 -0800

    Remove one tag

--- Secretive/ContentView.swift
+++ Secretive/ContentView.swift
@@ -4,28 +4,27 @@ import SecretKit
 struct ContentView: View {
     
     @ObservedObject var store: SecureEnclave.Store
-    @State var active: SecureEnclave.Secret?
+    @State var active: SecureEnclave.Secret.ID?
     
     @State var showingDeletion = false
     @State var deletingSecret: SecureEnclave.Secret?
     
     var body: some View {
         NavigationView {
-            List(selection: self.$active) {
+            List(selection: $active) {
                 Section(header: Text(store.name)) {
                     ForEach(store.secrets) { secret in
-                        NavigationLink(destination: SecretDetailView(secret: secret), tag: secret, selection: self.$active) {
+                        NavigationLink(destination: SecretDetailView(secret: secret), tag: secret.id, selection: self.$active) {
                             Text(secret.name)
                         }.contextMenu {
                             Button(action: { self.delete(secret: secret) }) {
                                 Text("Delete")
                             }
                         }
-                        .tag(secret)
                     }
                 }
             }.onAppear {
-                self.active = self.store.secrets.first
+                self.active = self.store.secrets.first?.id
             }
             .listStyle(SidebarListStyle())
             .frame(minWidth: 100, idealWidth: 240)
