7e5fc3cb246ca6be9a9859d05ad34acc0b184970
Max Goedjen <max.goedjen@gmail.com>
2020-03-07 00:00:09 -0800

    Cleanup new sheet

--- Secretive/CreateSecureEnclaveSecretView.swift
+++ Secretive/CreateSecureEnclaveSecretView.swift
@@ -2,40 +2,51 @@ import SwiftUI
 import SecretKit
 
 struct CreateSecureEnclaveSecretView: View {
-
+    
     @ObservedObject var store: SecureEnclave.Store
-
+    
     @State var name = ""
     @State var requiresAuthentication = true
-
+    
     var dismissalBlock: () -> ()
-
+    
     var body: some View {
-        Form {
-            Section(header: Text("Secret Name")) {
-                TextField("Name", text: $name)
-            }
-            Section {
-                Toggle(isOn: $requiresAuthentication) {
-                    Text("Requires Authentication (Biometrics or Password)")
-                }
-            }
-            Section {
-                HStack {
-                    Spacer()
-                    Button(action: dismissalBlock) {
-                        Text("Cancel")
+        VStack {
+            HStack {
+                Image(nsImage: NSApp.applicationIconImage)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .padding()
+                VStack {
+                    HStack {
+                        Text("Create a New Secret").bold()
+                        Spacer()
                     }
-                    Button(action: save) {
-                        Text("Create")
-                    }.disabled(name.isEmpty)
+                    HStack {
+                        Text("Name:")
+                        TextField("Name", text: $name)
+                    }
+                    HStack {
+                        Toggle(isOn: $requiresAuthentication) {
+                            Text("Requires Authentication (Biometrics or Password)")
+                        }
+                        Spacer()
+                    }
                 }
+                .onExitCommand(perform: dismissalBlock)
             }
-        }
-        .padding()
-        .onExitCommand(perform: dismissalBlock)
+            HStack {
+                Spacer()
+                Button(action: dismissalBlock) {
+                    Text("Cancel")
+                }
+                Button(action: save) {
+                    Text("Create")
+                }.disabled(name.isEmpty)
+            }
+        }.padding()
     }
-
+    
     func save() {
         try! store.create(name: name, requiresAuthentication: requiresAuthentication)
         dismissalBlock()
