5e76dd4024d27ce824feb515ee71129f373c67ff
Max Goedjen <max.goedjen@gmail.com>
2020-03-08 21:18:13 -0700

    Make name immutable

--- SecretKit/SmartCard/SmartCardSecret.swift
+++ SecretKit/SmartCard/SmartCardSecret.swift
@@ -6,7 +6,7 @@ extension SmartCard {
     public struct Secret: SecretKit.Secret {
         
         public let id: Data
-        public var name: String
+        public let name: String
         public let publicKey: Data
 
     }
