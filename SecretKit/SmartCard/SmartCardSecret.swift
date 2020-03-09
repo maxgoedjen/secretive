d0b9a0872c11a016b5046a5df436804ff9d57242
Max Goedjen <max.goedjen@gmail.com>
2020-03-08 21:19:47 -0700

    Trim

--- SecretKit/SmartCard/SmartCardSecret.swift
+++ SecretKit/SmartCard/SmartCardSecret.swift
@@ -4,7 +4,7 @@ import Combine
 extension SmartCard {
 
     public struct Secret: SecretKit.Secret {
-        
+
         public let id: Data
         public let name: String
         public let publicKey: Data
