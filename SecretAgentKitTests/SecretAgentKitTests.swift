4684d8a66979e024e1a493d651c44b0b671aa110
Max Goedjen <max.goedjen@gmail.com>
2020-03-15 16:23:42 -0700

    Adding test plan

--- SecretAgentKitTests/SecretAgentKitTests.swift
+++ SecretAgentKitTests/SecretAgentKitTests.swift
@@ -24,11 +24,4 @@ class SecretAgentKitTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
