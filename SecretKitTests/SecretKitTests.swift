017bdb85a4170b77c4dcaa6df679d03c31114b6c
Max Goedjen <max.goedjen@gmail.com>
2020-03-15 17:04:20 -0700

    Test infrastructure (#52)
    * Adding tests target
    
    * Adding test plan
    
    * Fixing deps
    
    * Add test workflow
    
    * Add config
    
    * More
    
    * Change workflow
    
    * Add test step to release
    
    * Signing changes
    
    * Signing changes
    
    * Remove 11.4 specific test
    
    * More signing

--- SecretKitTests/SecretKitTests.swift
+++ SecretKitTests/SecretKitTests.swift
@@ -24,11 +24,4 @@ class SecretKitTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
