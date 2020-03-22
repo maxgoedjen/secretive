104199cf533863352328f96900d493242e6003ee
Max Goedjen <max.goedjen@gmail.com>
2020-03-21 19:37:08 -0700

    Add test

--- SecretKitTests/AnySecretTests.swift
+++ SecretKitTests/AnySecretTests.swift
@@ -1,9 +1,17 @@
-//
-//  AnySecretTests.swift
-//  SecretKitTests
-//
-//  Created by Max Goedjen on 3/21/20.
-//  Copyright © 2020 Max Goedjen. All rights reserved.
-//
-
 import Foundation
+import XCTest
+@testable import SecretKit
+
+class AnySecretTests: XCTestCase {
+
+    func testEraser() {
+        let secret = SmartCard.Secret(id: UUID().uuidString.data(using: .utf8)!, name: "Name", algorithm: .ellipticCurve, keySize: 256, publicKey: UUID().uuidString.data(using: .utf8)!)
+        let erased = AnySecret(secret)
+        XCTAssert(erased.id == secret.id as AnyHashable)
+        XCTAssert(erased.name == secret.name)
+        XCTAssert(erased.algorithm == secret.algorithm)
+        XCTAssert(erased.keySize == secret.keySize)
+        XCTAssert(erased.publicKey == secret.publicKey)
+    }
+
+}
