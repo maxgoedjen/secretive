9a7af15f9ba03dddb9b993e2cea5185661a10bc4
Max Goedjen <max.goedjen@gmail.com>
2020-03-22 23:59:28 -0700

    Revert "Remove secretivetests"
    This reverts commit 5b7b2dcb680fecc3f4ed2c9eb05cd23f2db57a11.

---
+++ SecretiveTests/SecretiveTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import Secretive
+
+class SecretiveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+}
