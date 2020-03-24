11a5413a8cc216cfa764117080594fd477579844
Max Goedjen <max.goedjen@gmail.com>
2020-03-23 23:10:26 -0700

    That was dumb

--- SecretAgentKitTests/StubFileHandleReader.swift
+++ SecretAgentKitTests/StubFileHandleReader.swift
@@ -5,10 +5,10 @@ struct StubFileHandleReader: FileHandleReader {
 
     let availableData: Data
     var fileDescriptor: Int32 {
-        NSWorkspace.shared.runningApplications.filter({ $0.localizedName == "Xcode" }).first!.processIdentifier
+        NSWorkspace.shared.runningApplications.filter({ $0.localizedName == "SecretAgent" }).first!.processIdentifier
     }
     var pidOfConnectedProcess: Int32 {
-        NSWorkspace.shared.runningApplications.filter({ $0.localizedName == "Xcode" }).first!.processIdentifier
+        fileDescriptor
     }
 
 }
