5601d7eab8e88a9fbff1d2df4ad9a8b49d930a99
Max Goedjen <max.goedjen@gmail.com>
2020-09-18 14:05:31 -0700

    Button -> Link

--- Secretive/Views/NoStoresView.swift
+++ Secretive/Views/NoStoresView.swift
@@ -6,11 +6,7 @@ struct NoStoresView: View {
         VStack {
             Text("No Secure Storage Available").bold()
             Text("Your Mac doesn't have a Secure Enclave, and there's not a compatible Smart Card inserted.")
-            Button(action: {
-                NSWorkspace.shared.open(URL(string: "https://www.yubico.com/products/compare-yubikey-5-series/")!)
-            }) {
-                Text("If you're looking to add one to your Mac, the YubiKey 5 Series are great.")
-            }
+            Link("If you're looking to add one to your Mac, the YubiKey 5 Series are great.", destination: URL(string: "https://www.yubico.com/products/compare-yubikey-5-series/")!)
         }.padding()
     }
     
