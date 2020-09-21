8f74b5d72e15fc93931299624ae81aaec0a4492b
Max Goedjen <max.goedjen@gmail.com>
2020-09-20 18:56:24 -0700

    Nicer check

--- Secretive/Controllers/ApplicationDirectoryController.swift
+++ Secretive/Controllers/ApplicationDirectoryController.swift
@@ -6,17 +6,16 @@ struct ApplicationDirectoryController {
 extension ApplicationDirectoryController {
 
     var isInApplicationsDirectory: Bool {
-        #if DEBUG
-        return true
-        #else
         let bundlePath = Bundle.main.bundlePath
         for directory in NSSearchPathForDirectoriesInDomains(.applicationDirectory, .allDomainsMask, true) {
             if bundlePath.hasPrefix(directory) {
                 return true
             }
         }
+        if bundlePath.contains("/Library/Developer/Xcode") {
+            return true
+        }
         return false
-        #endif
     }
 
 }
