af479408a42691ccf58ca3332860b19cf3a3c6b3
Max Goedjen <max.goedjen@gmail.com>
2021-02-25 22:28:02 -0800

    Switch GitHub Actions releaser to use API key instead of app specific password (#212)
    * Update release.yml
    
    * Update signing.sh
    
    * Update release.yml
    
    * Fix weird spacing
    
    * Update signing.sh
    
    * Update release.yml
    
    * Update signing.sh

--- .github/scripts/signing.sh
+++ .github/scripts/signing.sh
@@ -10,10 +10,13 @@ security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k ci ci.keyc
 
 # Import Profiles
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
 echo $HOST_PROFILE_DATA | base64 -d -o Host.provisionprofile
 HOST_UUID=`grep UUID -A1 -a Host.provisionprofile | grep -io "[-A-F0-9]\{36\}"`
 cp Host.provisionprofile ~/Library/MobileDevice/Provisioning\ Profiles/$HOST_UUID.provisionprofile
 echo $AGENT_PROFILE_DATA | base64 -d -o Agent.provisionprofile
 AGENT_UUID=`grep UUID -A1 -a Agent.provisionprofile | grep -io "[-A-F0-9]\{36\}"`
 cp Agent.provisionprofile ~/Library/MobileDevice/Provisioning\ Profiles/$AGENT_UUID.provisionprofile
+
+# Create directories for ASC key
+mkdir ~/.private_keys
+echo -n "$APPLE_API_KEY_DATA" > ~/.private_keys/AuthKey_$APPLE_API_KEY_ID.p8
