54a4d2f9d5e8c97f553d26a83b5e4aeb021c2713
Max Goedjen <max.goedjen@gmail.com>
2021-11-07 17:33:16 -0800

    Delete unused

--- SecretKit/Common/Types/AuthenticationContextPersistenceStore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AuthenticationContextPersistenceStore.swift
-//  SecretKit
-//
-//  Created by Max Goedjen on 11/7/21.
-//  Copyright © 2021 Max Goedjen. All rights reserved.
-//
-
-import Foundation
