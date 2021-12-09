4f6f097873b97921b2cfed5f586e309ac3a12feb
Max Goedjen <max.goedjen@gmail.com>
2021-12-08 23:21:41 -0800

    WIP SPM

---
+++ Package.swift
@@ -0,0 +1,84 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Secretive",
+    platforms: [
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "SecretKit",
+            targets: ["SecretKit"]),
+        .library(
+            name: "SecretAgentKit",
+            targets: ["SecretKit"]),
+        .library(
+            name: "Brief",
+            targets: ["Brief"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .executableTarget(
+            name: "Secretive",
+            dependencies: ["SecretKit", "Brief"],
+            resources: [
+                .process("Resources/Assets.xcassets"),
+                .process("Resources/Credits.rtf"),
+                .process("Resources/InternetAccessPolicy.plist"),
+                .process("Resources/Secretive.entitlements"),
+            ],
+            // https://forums.swift.org/t/swift-package-manager-use-of-info-plist-use-for-apps/6532/8
+            linkerSettings: [.unsafeFlags( ["-sectcreate",
+                                            "__TEXT",
+                                            "__info_plist",
+                                            "Sources/SecretAgent/Resources/Info.plist"])
+                            ]
+        ),
+        .testTarget(
+            name: "SecretiveTests",
+            dependencies: ["Secretive"]),
+        .executableTarget(
+            name: "SecretAgent",
+            dependencies: ["SecretAgentKit", "Brief"],
+            resources: [
+                .process("Resources/Assets.xcassets"),
+                .process("Resources/InternetAccessPolicy.plist"),
+                .process("Resources/SecretAgent.entitlements")
+            ],
+            // https://forums.swift.org/t/swift-package-manager-use-of-info-plist-use-for-apps/6532/8
+            linkerSettings: [.unsafeFlags( ["-sectcreate",
+                                            "__TEXT",
+                                            "__info_plist",
+                                            "Sources/TesTest    SecretAgent/Resources/Info.plist"])
+                            ]
+        ),
+        .target(
+            name: "SecretKit",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SecretKitTests",
+            dependencies: ["SecretKit"]
+        ),
+        .target(
+            name: "SecretAgentKit",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SecretAgentKitTests",
+            dependencies: ["SecretAgentKit"])
+        ,
+        .target(
+            name: "Brief",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "BriefTests",
+            dependencies: ["Brief"]
+        ),
+    ]
+)
