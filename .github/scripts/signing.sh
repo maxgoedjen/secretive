53087c0439fa6a2cd9a48a28542afef87af03a88
Max Goedjen <max.goedjen@gmail.com>
2022-10-26 01:43:19 -0700

    Secrets duh

--- .github/scripts/signing.sh
+++ .github/scripts/signing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Create directories for ASC key
-mkdir .private_keys
-echo -n "$APPLE_API_KEY_DATA" > $(pwd)/.private_keys/AuthKey.p8
+mkdir ~/.private_keys
+echo -n "$APPLE_API_KEY_DATA" > ~/.private_keys/AuthKey.p8
