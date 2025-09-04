5186071a2907025652ce225c0c6f19e1ded0d82f
Max Goedjen <max.goedjen@gmail.com>
2025-09-03 23:34:58 -0700

    Add reveal in finder to copyable

--- Sources/Secretive/Controllers/URLs.swift
+++ Sources/Secretive/Controllers/URLs.swift
@@ -9,4 +9,17 @@ extension URL {
     static var socketPath: String {
         URL.agentHomeURL.appendingPathComponent("socket.ssh").path()
     }
+
+}
+
+extension String {
+
+    var normalizedPathAndFolder: (String, String) {
+        // All foundation-based normalization methods replace this with the container directly.
+        let processedPath = replacingOccurrences(of: "~", with: "/Users/\(NSUserName())")
+        let url = URL(filePath: processedPath)
+        let folder = url.deletingLastPathComponent().path()
+        return (processedPath, folder)
+    }
+
 }
