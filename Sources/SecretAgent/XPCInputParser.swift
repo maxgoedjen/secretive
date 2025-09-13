5cbe349705ce80362c073794c3ec6b0cefe7b8e4
Max Goedjen <max.goedjen@gmail.com>
2025-09-13 09:44:30 -0700

    Logging for xpc input parser

--- Sources/SecretAgent/XPCInputParser.swift
+++ Sources/SecretAgent/XPCInputParser.swift
@@ -2,18 +2,24 @@ import Foundation
 import SecretAgentKit
 import Brief
 import XPCWrappers
+import OSLog
 
 /// Delegates all agent input parsing to an XPC service which wraps OpenSSH
 public final class XPCAgentInputParser: SSHAgentInputParserProtocol {
 
+    private let logger = Logger(subsystem: "com.maxgoedjen.secretive.secretagent", category: "XPCAgentInputParser")
     private let session: XPCTypedSession<SSHAgent.Request, SSHAgentInputParser.AgentParsingError>
 
     public init() async throws {
+        logger.debug("Creating XPCAgentInputParser")
         session = try await XPCTypedSession(serviceName: "com.maxgoedjen.Secretive.SecretAgentInputParser", warmup: true)
+        logger.debug("XPCAgentInputParser is warmed up.")
     }
 
     public func parse(data: Data) async throws -> SSHAgent.Request {
-        try await session.send(data)
+        logger.debug("Parsing input")
+        defer { logger.debug("Parsed input") }
+        return try await session.send(data)
     }
 
     deinit {
