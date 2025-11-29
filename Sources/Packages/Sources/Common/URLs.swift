802a7e4512a11b51d5cf432397cdcf32de0aae24
Max Goedjen <max.goedjen@gmail.com>
2025-11-29 13:29:32 -0800

    Use separate socket for debug builds

--- Sources/Packages/Sources/Common/URLs.swift
+++ Sources/Packages/Sources/Common/URLs.swift
@@ -2,19 +2,23 @@ import Foundation
 
 extension URL {
 
-    static var agentHomeURL: URL {
+    public static var agentHomeURL: URL {
         URL(fileURLWithPath: URL.homeDirectory.path().replacingOccurrences(of: Bundle.hostBundleID, with: Bundle.agentBundleID))
     }
 
-    static var socketPath: String {
+    public static var socketPath: String {
+        #if DEBUG
+        URL.agentHomeURL.appendingPathComponent("socket-debug.ssh").path()
+        #else
         URL.agentHomeURL.appendingPathComponent("socket.ssh").path()
+        #endif
     }
 
 }
 
 extension String {
 
-    var normalizedPathAndFolder: (String, String) {
+    public var normalizedPathAndFolder: (String, String) {
         // All foundation-based normalization methods replace this with the container directly.
         let processedPath = replacingOccurrences(of: "~", with: "/Users/\(NSUserName())")
         let url = URL(filePath: processedPath)
