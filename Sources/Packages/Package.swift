9b203fce0e823d42c4b58da2263dd193bf2c2112
Max Goedjen <max.goedjen@gmail.com>
2026-09-07 14:32:57 -0700

    Bumping to macOS 15 + set workaround for spm arm64e

--- Sources/Packages/Package.swift
+++ Sources/Packages/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.2
+// swift-tools-version:6.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SecretivePackages",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v15)
     ],
     products: [
         .library(
